ea29f9b79ea53ec72806646b490c938847915230
livelaughconquer <jorgecfelix@gmail.com>
2015-10-03 17:44:42 -0700

    Calculator Android Folder
    Formatted for Emulation on Nexus 6

---
+++ Calculator/app/src/main/java/com/example/jorge/calculator/Calculator.java
@@ -0,0 +1,135 @@
+package com.example.jorge.calculator;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+
+public class Calculator extends ActionBarActivity {
+    // Create an array of all button IDs
+    private int[] numericButtons = {
+            R.id.btn0,
+            R.id.btn1,
+            R.id.btn2,
+            R.id.btn3,
+            R.id.btn4,
+            R.id.btn5,
+            R.id.btn6,
+            R.id.btn7,
+            R.id.btn8,
+            R.id.btn9};
+    //Create an array of all Operator buttons
+    private int[] operatorButtons = {
+            R.id.btnAdd,
+            R.id.btnSubtract,
+            R.id.btnMultiply,
+            R.id.btnDivide};
+    private TextView txtScreen;
+    private boolean lastNumeric;
+    private boolean stateError;
+    private boolean lastDot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+        this.txtScreen = (TextView) findViewById(R.id.txtscreen);
+        setNumericOnClickListener();
+        setOperatorOnClickListener();
+    }
+
+    private void setNumericOnClickListener() {
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                if (stateError) {
+                    txtScreen.setText(button.getText());
+                    stateError = false;
+                } else {
+                    txtScreen.append(button.getText());
+                }
+                lastNumeric = true;
+            }
+        };
+
+        for (int id : numericButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+    }
+
+    private void setOperatorOnClickListener() {
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (lastNumeric && !stateError) {
+                    Button button = (Button) v;
+                    txtScreen.append(button.getText());
+                    lastNumeric = false;
+                    lastDot = false;    // Reset the DOT flag
+                }
+            }
+        };
+
+        for (int id : operatorButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+
+        findViewById(R.id.btnDecimal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (lastNumeric && !stateError && !lastDot) {
+                    txtScreen.append(".");
+                    lastNumeric = false;
+                    lastDot = true;
+                }
+            }
+        });
+
+        findViewById(R.id.btnClear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtScreen.setText("");
+                lastNumeric = false;
+                stateError = false;
+                lastDot = false;
+            }
+        });
+        findViewById(R.id.btnEquals).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onEqual();
+            }
+        });
+    }
+
+    private void onEqual() {
+
+        if (lastNumeric && !stateError) {
+            String txt = txtScreen.getText().toString();
+            Expression expression = new ExpressionBuilder(txt).build();
+            try {
+                double result = expression.evaluate();
+                txtScreen.setText(Double.toString(result));
+                lastDot = true;
+            } catch (ArithmeticException ex) {
+                txtScreen.setText("Error");
+                stateError = true;
+                lastNumeric = false;
+            }
+        }
+    }
+}
+
+
+
+
+
